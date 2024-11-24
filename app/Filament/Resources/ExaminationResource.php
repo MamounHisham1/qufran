b1dfd1c514608218d853c195aa1c8631368b05ac
Mamoun Hisham <mamounprogrammer@gmail.com>
2024-11-24 21:09:01 +0200

    starting on exams

---
+++ app/Filament/Resources/ExaminationResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExaminationResource\Pages;
+use App\Filament\Resources\ExaminationResource\RelationManagers;
+use App\Models\Examination;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExaminationResource extends Resource
+{
+    protected static ?string $model = Examination::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('correct_answer')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+                Forms\Components\DatePicker::make('start_at')
+                    ->required(),
+                Forms\Components\DatePicker::make('end_at'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('correct_answer')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('start_at')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end_at')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExaminations::route('/'),
+            'create' => Pages\CreateExamination::route('/create'),
+            'edit' => Pages\EditExamination::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2024_11_24_170259_create_examinations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('examinations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('correct_answer');
+            $table->longText('description')->nullable();
+            $table->date('start_at')->default(now()->format('Y-m-d-H'));
+            $table->date('end_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('examinations');
+    }
+};
