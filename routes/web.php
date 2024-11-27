5a5b16d84aeea54c982d487fa8f3412fdf5f872e
Mamoun Hisham <mamounprogrammer@gmail.com>
2024-11-27 20:17:59 +0200

    created invokable class for the home page

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('home');
-})->name('dashboard');
+Route::get('/', HomeController::class)->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
